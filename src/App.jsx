54a12cd9e732e29cded7066a27b6406dbe9a350e
souk <souksavanh.sy@gmail.com>
2021-07-27 12:09:53 +0200

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class App extends PureComponent {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h1>Hello!</h1>;
+  }
+}
+
+export default App;
