6032156e797cb9db80dd5e85032f2b3af7aa4977
souk <souksavanh.sy@gmail.com>
2021-07-30 13:32:04 +0200

    Add filter function

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,3 +14,91 @@ scrollerButton.addEventListener("click", (event) => {
   const target = document.querySelector(href);
   target.scrollIntoView({ behavior: "smooth" });
 });
+
+// Tags
+let cacheData = null;
+let activeTags = [];
+const tags = document.querySelectorAll(".nav__tag");
+tags.forEach((tag) => {
+  tag.addEventListener("click", (event) => {
+    event.preventDefault();
+    const classes = event.target.classList;
+    const href = event.target.getAttribute("href").toLowerCase();
+    const active = classes.contains("nav__tag--active");
+    if (!active) {
+      classes.add("nav__tag--active");
+      activeTags = [...activeTags, href];
+    } else {
+      classes.remove("nav__tag--active");
+      activeTags = activeTags.filter((tag) => tag !== href);
+    }
+
+    filterAuthors();
+  });
+});
+
+// Filter by tags and render new author list
+async function filterAuthors() {
+  let authorsList = document.querySelector(".authors__list");
+
+  // Clear up author list children
+  while (authorsList.firstChild) {
+    authorsList.removeChild(authorsList.lastChild);
+  }
+
+  // Wait for data
+  await loadData("api/fisheyeData.json");
+
+  // Render new author list
+  const selected = activeTags.reduce((group, tag) => {
+    const hasTag = cacheData.filter((author) => author.tags.includes(tag));
+    return [...new Set(group.concat(hasTag))];
+  }, []);
+
+  selected.forEach((author) => {
+    const component = renderAuthor(author);
+    authorsList.insertAdjacentHTML("beforeend", component);
+  });
+}
+
+// Call API once and cache data until refresh page
+async function loadData(url) {
+  if (!cacheData) {
+    const res = await fetch(url);
+    const { photographers } = await res.json();
+    cacheData = [...photographers];
+  }
+}
+
+// Author component
+function renderAuthor({ name, portrait, city, country, tagline, price, tags }) {
+  return `
+  <article class="author">
+  <div class="author__cell">
+    <a href="${name}" class="author__link" aria-label="${name}">
+      <div class="author__portrait">
+        <img src="./images/authors/${portrait}" alt="${name}">
+      </div>
+      <h2 class="author__name">${name}</h2>
+    </a>
+    <div class="author__info">
+      <p class="author__location">${city}, ${country}</p>
+      <p class="author__tagline">${tagline}</>
+      <p class="author__price">${price}€/jour</p>
+    </div>
+    <ul class="tags author__tags">
+      ${tags.forEach((tag) => renderTag(tag))}
+    </ul>
+  </div>
+</article>
+  `;
+}
+
+// render
+function renderTag(tag) {
+  return `
+      <li>
+        <a href="" class="tag author__tag" aria-label="Tag ${tag}}}">#${tag}</a>
+      </li>
+  `;
+}
