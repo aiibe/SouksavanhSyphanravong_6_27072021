a845a7dbac81d2fe9a7cc43c81d43a5c579c6e02
aiibe <souksavanh.sy@gmail.com>
2021-08-14 18:27:43 +0200

    Fix app missing base url

--- src/PageFactory.js
+++ src/PageFactory.js
@@ -5,11 +5,14 @@ import Profile from "./pages/Profile";
 import dataStore from "./stores/dataStore";
 
 class PageFactory {
+  constructor(baseURL){
+    this.baseURL = baseURL
+  }
   create(path){
     switch (path) {
-      case "/":
+      case this.baseURL + "/":
         return new Home("#app");
-      case "/profile/":
+      case this.baseURL + "/profile/":
         return dataStore.subscribe(new Profile("#app"));
       default:
         return new Error404('#app')
